c6568be0834ab4416ad4b67d2c6ea7d3e77c276b
Kavya Mittal <mister1729x@gmail.com>
2025-02-09 16:51:39 +0530

    done

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use((req,res,next)=>{
 
 //     let registeredUser = await User.register(fakeUser,"helloworld");
 //     res.send(registeredUser);
+ // so this is the completed nodejs application
 // })
 
 main()
